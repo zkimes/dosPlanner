795c9931d0e073cc444dd8bbf6d637d616da4c2f
zkimes <zkimes@gmail.com>
2015-11-01 16:09:41 -0600

    yeoman project scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dosPlannerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dosPlannerApp
+ */
+angular.module('dosPlannerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dosPlannerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dosPlannerApp
+ */
+angular.module('dosPlannerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
